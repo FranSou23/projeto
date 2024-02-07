fe845a8556707f0105146d93a9fd72b63ab1e69e
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-02-07 19:44:57 -0300

    projeto

---
+++ app.js
@@ -0,0 +1,13 @@
+function abrirsalame() {
+    // Abrir a página queijo.html em uma nova janela ou guia
+    window.open('salame.html', '_blank');
+    // Fechar a janela ou guia atual (pode não funcionar em alguns navegadores devido a restrições de segurança)
+    window.close();
+}
+function abrirqueijo() {
+    window.open('queijo.html', '_blank');
+    window.close();
+}
+function abrirpimenta() {
+    window.open('pimenta.html', '_blank');
+}
